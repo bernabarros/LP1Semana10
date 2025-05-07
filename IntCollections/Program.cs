534934784afc2d757d8057cb93ecfef24168cacc
bernabarros <bernardobarros.work@gmail.com>
2025-05-07 21:55:00 +0100

    Add IntCollections code

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 
 namespace IntCollections
 {
@@ -6,7 +9,41 @@ namespace IntCollections
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            List<int>list = new List<int>();
+            Stack<int>stack = new Stack<int>();
+            Queue<int>queue = new Queue<int>();
+            HashSet<int>hashset = new HashSet<int>();
+
+
+            int[] inputs = new int[] {1, 10, -30, 10, -5};
+
+            foreach(int number in inputs)
+            {
+                list.Add(number);
+            }
+
+            foreach(int number in inputs)
+            {
+                stack.Push(number);
+            }
+
+            foreach(int number in inputs)
+            {
+                queue.Enqueue(number);
+            }
+
+            foreach(int number in inputs)
+            {
+                hashset.Add(number);
+            }
+
+            Console.WriteLine("List: " + String.Join(", ", list));
+
+            Console.WriteLine("Stack : " + String.Join(", ", stack));
+
+            Console.WriteLine("Queue : " + String.Join(", ", queue));
+            
+            Console.WriteLine("Hashset : " + String.Join(", ", hashset));
         }
     }
 }
